7b5be499aa4fbcd0d82acb4c342665cf84f5bdf7
JozsefCs <81176906+JozsefCs@users.noreply.github.com>
2021-04-07 14:17:14 +0200

    emailIsExists

--- beadando_szfm/JavaFX11/src/main/java/hu/unideb/inf/FXMLBelepes.java
+++ beadando_szfm/JavaFX11/src/main/java/hu/unideb/inf/FXMLBelepes.java
@@ -6,8 +6,11 @@ import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
-public class FXMLBelepes {
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
+public class FXMLBelepes {
     @FXML
     private TextField login_username;
 
@@ -31,16 +34,37 @@ public class FXMLBelepes {
 
     @FXML
     void handleBejelentkezekButtonPushed(ActionEvent event) {
+        ArrayList<Users> usersList = new ArrayList<>();
+        boolean emailIsExists = false;
+        MainApp app = new MainApp();
         String login_user_name = login_username.getText();
         String jelszo = login_password.getText();
         System.out.println(login_user_name);
-        System.out.println();
+        System.out.println(jelszo);
 
+        
+        //Létezik-e ilyen e-mail////////////////////////////////////////
+        for (Users user: usersList) {//végigmeghy a lista elemein
+            if(app.checkEmailInUse(login_user_name, user.getEmail()))//megnézi hogy egyenlő-e a két string
+                emailIsExists = true;
+        }
+        if(emailIsExists) {
+            System.out.println("Sikeres belépés");
+            login_error.setText("Sikeres belépés");
+        }else {
+            System.out.println("Nincs ilyen email regisztrálva");
+            login_error.setText("Nincs ilyen email regisztrálva");
+        }
+        /////////////////////////////////////////////////////////////
     }
 
     @FXML
     void handleRegisztrálokButtonPushed(ActionEvent event) {
         String registration_user_name = registration_username.getText();
         System.out.println(registration_user_name);
+    }
+
+    public TextField getLogin_username() {
+        return login_username;
     }
 }
