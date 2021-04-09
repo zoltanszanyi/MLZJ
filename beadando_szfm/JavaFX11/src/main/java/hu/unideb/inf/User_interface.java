fded90e4a48270e289341203f3427c4fe4002330
zoltanszanyi <81174081+zoltanszanyi@users.noreply.github.com>
2021-04-09 15:52:03 +0200

    User_interface

---
+++ beadando_szfm/JavaFX11/src/main/java/hu/unideb/inf/User_interface.java
@@ -0,0 +1,66 @@
+package hu.unideb.inf;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
+public class User_interface {
+
+    @FXML
+    private ComboBox<?> SelectLocation;
+
+    @FXML
+    private Label ActualLocation;
+
+    @FXML
+    private Label LocationInfo;
+
+    @FXML
+    private Label Fullness;
+
+    @FXML
+    private Label Price;
+
+    @FXML
+    private ComboBox<?> SelectBicycle;
+
+    @FXML
+    private ComboBox<?> SelectHours;
+
+    @FXML
+    private Label PrintReserveTime;
+
+    @FXML
+    private Label Reserve;
+
+    @FXML
+    private ComboBox<?> SelectMin;
+
+    @FXML
+    void HandleReserveButton(MouseEvent event) {
+
+    }
+
+    @FXML
+    void HandleSelectBicycle(ActionEvent event) {
+
+    }
+
+    @FXML
+    void HandleSelectHours(ActionEvent event) {
+
+    }
+
+    @FXML
+    void HandleSelectLocation(ActionEvent event) {
+
+    }
+
+    @FXML
+    void HandleSelectMin(ActionEvent event) {
+
+    }
+
+}
